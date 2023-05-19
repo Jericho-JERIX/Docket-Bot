ee186c352439eb0bac23e0395d0402bf858b314d
KanonKC <kanonkce@gmail.com>
2023-05-20 02:46:29 +0700

    Finally found graph lib

--- .test.ts
+++ .test.ts
@@ -1,3 +1,71 @@
-import { Ping } from "./src/interaction/commands/ping";
+const ChartJSImage = require("chart.js-image");
 
-console.log(Ping);
+const line_chart = ChartJSImage()
+	.chart({
+		type: "bar",
+		data: {
+			labels: [
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+			],
+			datasets: [
+				{
+					data: [65, 59, 80, 81, 56, 55, 40],
+					backgroundColor: [
+						"rgba(255, 99, 132, 0.2)",
+						"rgba(255, 159, 64, 0.2)",
+						"rgba(255, 205, 86, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(54, 162, 235, 0.2)",
+						"rgba(153, 102, 255, 0.2)",
+						"rgba(201, 203, 207, 0.2)",
+					],
+					borderColor: [
+						"rgb(255, 99, 132)",
+						"rgb(255, 159, 64)",
+						"rgb(255, 205, 86)",
+						"rgb(75, 192, 192)",
+						"rgb(54, 162, 235)",
+						"rgb(153, 102, 255)",
+						"rgb(201, 203, 207)",
+					],
+					borderWidth: 1,
+				},
+			],
+		},
+		options: {
+			title: {
+				display: true,
+				text: "Chart.js Line Chart",
+			},
+			scales: {
+				xAxes: [
+					{
+						scaleLabel: {
+							display: true,
+							labelString: "Month",
+						},
+					},
+				],
+				yAxes: [
+					{
+						stacked: true,
+						scaleLabel: {
+							display: true,
+							labelString: "Value",
+						},
+					},
+				],
+			},
+		},
+	}) // Line chart
+	.backgroundColor("white")
+	.width(500) // 500px
+	.height(300); // 300px
+
+line_chart.toFile("chart.png"); // Promise<()>
