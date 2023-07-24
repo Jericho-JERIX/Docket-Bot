23f1d13f4788d9a72c18226e7f38ed4e23e02dcc
KanonKC <kanonkce@gmail.com>
2023-07-24 11:05:04 +0700

    Hotfix: Modified label cannot be more than 100 char

--- src/templates/components/ClearedHomeworkCard.ts
+++ src/templates/components/ClearedHomeworkCard.ts
@@ -7,6 +7,12 @@ import { DocketHomework } from "../../types/services/HomeworkServiceType";
 export function ClearedHomeworkCard(homework: DocketHomework): string {
 	const hw: PopulatedDocketHomework = populateDocketHomework(homework);
 
+	let modifiedLabel = hw.label;
+
+	if (hw.label.length > 99) {
+		modifiedLabel = hw.label.slice(0, 96) + "...";
+	}
+
 	const shorthenDayName =
 		ShorthenDayName[
 			hw.day_name.toUpperCase() as keyof typeof ShorthenDayName
@@ -17,7 +23,7 @@ export function ClearedHomeworkCard(homework: DocketHomework): string {
 			hw.month,
 			2,
 			"0"
-		)}] ${hw.alert_icon} (เดี๋ยวนี้!) ${hw.type_icon} ${hw.label}`;
+		)}] ${hw.alert_icon} (เดี๋ยวนี้!) ${hw.type_icon} ${modifiedLabel}`;
 	} else {
 		return `[${shorthenDayName}.${fixSpace(hw.date, 2, "0")}/${fixSpace(
 			hw.month,
@@ -25,6 +31,6 @@ export function ClearedHomeworkCard(homework: DocketHomework): string {
 			"0"
 		)}] ${hw.alert_icon} (${fixSpace(hw.day_left, 3)} วัน) ${
 			hw.type_icon
-		} ${hw.label}`;
+		} ${modifiedLabel}`;
 	}
 }
