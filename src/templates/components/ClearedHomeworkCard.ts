2c9ff4e5f4903537590191192da4bfe2ba1ecba5
KanonKC <kanonkce@gmail.com>
2023-07-24 11:08:32 +0700

    Fix: mis fix

--- src/templates/components/ClearedHomeworkCard.ts
+++ src/templates/components/ClearedHomeworkCard.ts
@@ -7,30 +7,30 @@ import { DocketHomework } from "../../types/services/HomeworkServiceType";
 export function ClearedHomeworkCard(homework: DocketHomework): string {
 	const hw: PopulatedDocketHomework = populateDocketHomework(homework);
 
-	let modifiedLabel = hw.label;
-
-	if (hw.label.length > 99) {
-		modifiedLabel = hw.label.slice(0, 96) + "...";
-	}
-
 	const shorthenDayName =
 		ShorthenDayName[
 			hw.day_name.toUpperCase() as keyof typeof ShorthenDayName
 		];
 
+	let result: string = "";
 	if (hw.day_left == 0) {
-		return `[${shorthenDayName}.${fixSpace(hw.date, 2, "0")}/${fixSpace(
+		result = `[${shorthenDayName}.${fixSpace(hw.date, 2, "0")}/${fixSpace(
 			hw.month,
 			2,
 			"0"
-		)}] ${hw.alert_icon} (เดี๋ยวนี้!) ${hw.type_icon} ${modifiedLabel}`;
+		)}] ${hw.alert_icon} (เดี๋ยวนี้!) ${hw.type_icon} ${hw.label}`;
 	} else {
-		return `[${shorthenDayName}.${fixSpace(hw.date, 2, "0")}/${fixSpace(
+		result = `[${shorthenDayName}.${fixSpace(hw.date, 2, "0")}/${fixSpace(
 			hw.month,
 			2,
 			"0"
 		)}] ${hw.alert_icon} (${fixSpace(hw.day_left, 3)} วัน) ${
 			hw.type_icon
-		} ${modifiedLabel}`;
+		} ${hw.label}`;
 	}
+
+	if (result.length > 99) {
+		result = result.slice(0, 96) + "...";
+	}
+	return result;
 }
