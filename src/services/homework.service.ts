b0018cdd3902ec92727fe1232e370ee35f999816
KanonKC <kanonkce@gmail.com>
2024-10-04 19:41:13 +0700

    Create/Get-All Homeworks

--- src/services/homework.service.ts
+++ src/services/homework.service.ts
@@ -3,20 +3,23 @@ import { BACKEND_URL } from "../constants/service";
 import { HomeworkServiceType } from "../types/services/HomeworkServiceType";
 import { HomeworkType } from "../constants/homework";
 import { prisma } from "../database/prisma";
-import { HomeworkChannel, HomeworkFile } from "@prisma/client";
+import { Homework, HomeworkChannel, HomeworkFile } from "@prisma/client";
+import { yearDecider } from "../util/YearDecider";
+import { searchHomework } from "../util/SearchHomework";
 
 const MaxTimestamp = 9999999999;
+const DELTA_TIME_SECOND = 0
 
 async function homeworkManageAuth(discord_id: string, channel: HomeworkChannel & { homework_file: HomeworkFile }) {
 
     if (!channel) {
-        throw new Error("Channel not found");
+        return { status: 404 }
     }
 
     const file = channel.homework_file;
 
     if (!channel.can_edit && file.owner_id !== discord_id) {
-        throw new Error("You are not the owner of this file");
+        return { status: 401 }
     }
 
 }
@@ -30,7 +33,7 @@ export const HomeworkService: HomeworkServiceType = {
         })
 
         if (!channel) {
-            throw new Error("Channel not found");
+            return { status: 404 }
         }
 
         await homeworkManageAuth(discord_id, channel);
@@ -43,31 +46,88 @@ export const HomeworkService: HomeworkServiceType = {
                     month: 0,
                     year: 0,
                     timestamp: MaxTimestamp,
-                    day_name: "Snuday",
+                    day_name: "Sunday",
                     no_deadline: true,
                     ...body,
                 }
             })
         }
-        
-        const dateTS = new Date(body.year, body.month - 1, body.date);
 
+        let decidedYear: number
+        let timestamp: number;
+        try {
+            decidedYear = yearDecider(body.date, body.month);
+            timestamp = new Date(
+                decidedYear,
+                body.month - 1,
+                body.date,
+                23, 59, 59
+            ).getTime();
+        }
+        catch (e) {
+            return { status: 400 }
+        }
+
+        const finalTimestamp = timestamp + DELTA_TIME_SECOND
+        return prisma.homework.create({
+            data: {
+                file_id: channel.file_id,
+                timestamp: finalTimestamp,
+                day_name: new Date(finalTimestamp).toLocaleDateString("en-US", { weekday: "long" }),
+                no_deadline: false,
+                date: body.date,
+                month: body.month,
+                year: decidedYear,
+                type: body.type,
+                label: body.label,
+            }
+        })
 	},
 	getAll: async (channel_id, type, keyword) => {
 		if (!type) type = HomeworkType.ALL;
 		if (!keyword) keyword = "";
-		return axios
-			.get(
-				`${BACKEND_URL}/channel/${channel_id}` +
-					`?type=${type.toUpperCase()}` +
-					`&keyword=${keyword}`
-			)
-			.then((res) => {
-				return res;
-			})
-			.catch((err) => {
-				return err.response;
-			});
+		
+        const channel = await prisma.homeworkChannel.findUnique({ where: { channel_id }, include: { homework_file: true } })
+
+        if (!channel) {
+            return { status: 400 }
+        }
+
+        const totalHomeworksCount = await prisma.homework.count({
+            where: {
+                file_id: channel.file_id,
+            }
+        })
+        let filteredHomeworkCount = totalHomeworksCount
+
+        let homeworks: Homework[] = []
+
+        if (type === HomeworkType.ALL) {
+            homeworks = await prisma.homework.findMany({
+                where: {
+                    file_id: channel.file_id,
+                }
+            })
+        } else {
+            homeworks = await prisma.homework.findMany({
+                where: {
+                    file_id: channel.file_id,
+                    type: type,
+                }
+            })
+            filteredHomeworkCount = homeworks.length
+        }
+
+        const filteredHomeworks = await searchHomework(homeworks, keyword);
+        filteredHomeworkCount = filteredHomeworks.length
+
+        return {
+            file: channel.homework_file,
+            total_homework_count: totalHomeworksCount,
+            type_homework_count: filteredHomeworkCount,
+            homeworks: filteredHomeworks,
+        }
+
 	},
 	update: async (discord_id, channel_id, homework_id, body) => {
 		return axios
